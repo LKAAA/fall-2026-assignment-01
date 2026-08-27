7c7ace4e03096997316dec0b20be27251c4de953
LKAAA <hbridgesiscool05@gmail.com>
2026-08-27 13:10:04 -0400

    Exercise 8

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,23 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private listeners: { [K in keyof T]?: ((data: any) => void)[] } = {}
+  
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+    this.listeners[eventName]!.push(callback);
+  }
+
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const callbacks = this.listeners[eventName];
+    if (callbacks) {
+      callbacks.forEach((callbacks) => callbacks(data));
+    }
+  }
 }
