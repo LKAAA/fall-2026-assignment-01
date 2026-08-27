a9aefdb1e8cb2bfbf54005e8b47863a4a4a91b7d
LKAAA <hbridgesiscool05@gmail.com>
2026-08-27 14:48:36 -0400

    Exercise 10

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,55 @@
-type UserAccount = {};
+type UserAccount = {
+  id: string,
+  createdAt: Date,
+  email: string,
+  passwordHash: string,
+  profile: {
+    bio: string,
+    avatarUrl: string,
+  },
+};
+
+// UserAccount containing the following fields: id (string), 
+// createdAt (Date), email (string), passwordHash (string), 
+// and a nested profile object containing a bio (string) and avatarUrl (string).
 
 export class UserRegistry {
-  public registerUser(
+
+private users: UserAccount[] = [];
+
+public registerUser(
     data: Omit<UserAccount, 'id' | 'createdAt'>,
   ): UserAccount {
-    return {};
+    const newUser: UserAccount = {
+      ...data,
+      id: generateRandomString(10),
+      createdAt: new Date(),
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
   }
 
   public getUserView(
     id: string,
   ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+    const foundUser = this.users.find(user => user.id == id);
+
+    return foundUser ? 
+    Object.freeze({id: foundUser.id, email: foundUser.email, profile: Object.freeze(foundUser.profile)}) 
+    : undefined;
   }
+}
+
+function generateRandomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  
+  return result;
 }
