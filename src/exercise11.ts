1f40e0c6dd423ac207470b7119269488a0212786
LKAAA <hbridgesiscool05@gmail.com>
2026-09-02 12:51:00 -0400

    Exercise 11 and 12 + some cleanup

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,20 @@
+import fs from 'fs/promises';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+
+  let fileData: string = ""
+
+  const timestamp = new Date().toISOString();
+  const timestampedMessage = `${timestamp} ${statusMessage}\n`;
+
+  try {
+    await fs.appendFile(filePath, timestampedMessage, 'utf-8');
+    console.log("Status message written successfully.");
+  } catch (error) {
+    console.error('Error writing file.', error);
+    throw error;
+  }
+}
--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,20 @@
+interface UserProfile {
+  id: number;
+  name: string;
+  email: string;
+}
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+
+  const response: Response = await fetch('https://jsonplaceholder.typicode.com/users');
+  
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  
+  const users: UserProfile[] = await response.json() as UserProfile[];
+
+  const emails: string[] = users.map((user) => user.email);
+
+  return emails;
 }
