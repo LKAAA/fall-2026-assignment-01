d0f8a59876564a0460a21f1403d84f1a2fee95a0
LKAAA <hbridgesiscool05@gmail.com>
2026-09-02 13:11:35 -0400

    Exercise 13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,31 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+
+  try {
+    const response: Response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+    const data = (await response.json()) as TodoItem;
+    console.log(data);
+    return data;
+
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Caught Expected Error", error.message);
+    } else {
+      console.error("Caught Unexpected Error", error);
+    }
+    return null;
+  }
+
+
+  
 }
