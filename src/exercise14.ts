1477c7774b4bce23a607f2b687d7f5d442032ea6
LKAAA <hbridgesiscool05@gmail.com>
2026-09-02 13:51:53 -0400

    Exercise 14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,23 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number,
+  title: string,
+  body: string,
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
+  try {
+    const fetchPromises = postIds.map(id =>
+      fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => res.json())
+    );
+
+    const todosArray: PostItem[] = await Promise.all(fetchPromises) as PostItem[];
+
+    return todosArray;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; 
+  }
+
+
   return [];
 }
